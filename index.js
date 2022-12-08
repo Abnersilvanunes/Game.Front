2f166f044dc6760c28061e8d8c55e15697930fc4
Abnersilvanunes <88353672+Abnersilvanunes@users.noreply.github.com>
2022-12-08 07:11:39 -0300

    Game.Front

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 
 const app = express();
 const axios = require('axios').default;
@@ -6,47 +7,36 @@ const axios = require('axios').default;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//MAPEAMENTO DA PASTA PUBLIC
 app.use(express.static('public'));
-
-//CONFIGURA O EJS COMO VIEW ENGINE (REDENRIZA AS PÁGINAS DE FRONT-END)
 app.set('view engine', 'ejs');
 
 
-//ROTA DE CADASTRO DE CATEGORIAS
 app.get('/cadastroCategorias', (req, res)=>{
     res.render('categoria/index');
 });
-
-//ROTA DE LISTAGEM DE CATEGORIAS
+S
 app.get('/listagemCategorias', (req, res)=>{
     
     const urlListagemCategoria = 'http://localhost:3000/listarCategoria';
 
-    /*
-    CHAMADA PELO AXIOS:
-    1 - URL DA ROTA (urlListagemCategoria)
-    2 - CALLBACK DA RESPOSTA DA CHAMADA
-    */
+    
     axios.get(urlListagemCategoria)
         .then(
             (response)=>{
-                // console.log(response.data);
-                // res.send(response.data);
                 let categorias = response.data;
                 res.render('categoria/listagemCategoria', {categorias});
 
         }); 
     });
 
-    //ROTA DE LISTAGEM DE EDIÇÃO
+    
     app.get('/formEdicaoCategorias/:id', (req, res)=>{
         
-        //RECEBE O ID DE CATEGORIA QUE VAI SER EDITADO
+        
         let {id} = req.params;
-        // console.log(id);
+       
 
-        //CHAMADA DO AXIOS PARA A API:
+
         const urlListagemCategoria = `http://localhost:3000/listarCategoria/${id}`;
         
         axios.get(url)
@@ -60,7 +50,6 @@ app.get('/listagemCategorias', (req, res)=>{
         )
     });
 
-    //ROTA DE EDIÇÃO
     app.post('/alterarCategoria', (req, res)=>{
 
         const urlAlterarCategoria = 'http://localhost:3000/alterarCategoria';
